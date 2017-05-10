ed27bce6b03fc9b1d2b42856c911d3750ee6e1d8
Rasmus <mail@rasmusronit.me>
2017-05-10 10:24:03 +0200

    Update navbar and add page navigation function
    Upload the header and index files, as well as the stylesheet to update the theme to have a better looking navbar and functioning page navigation.

--- ronit-theme/index.php
+++ ronit-theme/index.php
@@ -1,5 +1,23 @@
 <?php get_header(); ?>
 
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tempor, sem eu suscipit pharetra, dolor justo luctus orci, sed tristique dolor ligula vitae justo. Nulla fringilla euismod nisi vitae mollis. Aenean id nunc nec nunc dictum placerat ut vitae lacus. Fusce quis nibh quis ex cursus placerat vitae vitae eros. Nam vulputate pharetra elit, non gravida tortor interdum eu. Curabitur in accumsan nulla. Vivamus urna dui, iaculis vel iaculis sit amet, egestas eget elit. Nullam convallis mauris at malesuada maximus. Etiam rhoncus interdum tristique. Vivamus vestibulum libero ac venenatis sodales. Pellentesque ex purus, vulputate et purus in, pharetra gravida nisi. Nulla sed fringilla tortor, ut tincidunt justo.</p>
+<div id="page-content"
+    <?php
+    
+    if( have_posts() ):
+
+        while( have_posts() ): the_post(); ?>
+
+
+        <h2><?php the_title(); ?></h2>
+
+        <p><?php the_content(); ?></p>
+
+        <?php endwhile;
+        
+    endif;
+
+    ?>
+
+</div>
 
 <?php get_footer(); ?>
